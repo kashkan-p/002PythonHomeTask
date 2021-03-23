988b5462d205c9ac4b251b8e7ec42cb53e1392eb
pvl <kashkan.p@gmail.com>
2021-03-23 18:31:43 +0300

    Edited fixtures

--- fixtures/rabota_by_fixtures.py
+++ fixtures/rabota_by_fixtures.py
@@ -1,6 +1,11 @@
 import pytest
 from clients.http_client import HttpClient
 
+
+URL = 'https://rabota.by/search/vacancy'
+KEYWORD = "python"
+PYTHON_QUERY_PARAMS = {"area": "1002", "text": KEYWORD}
+SHOTGUN_QUERY_PARAMS = {"area": "1002", "text": "shotgun"}
 HEADER = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) '
                         'Chrome/39.0.2171.95 Safari/537.36'}
 
@@ -12,9 +17,9 @@ def client():
 
 @pytest.fixture
 def get_response_code(client):
-    return client.get("https://rabota.by/search/vacancy?text=python", header=HEADER).status_code
+    return client.get(URL, header=HEADER).status_code
 
 
 @pytest.fixture
 def search_result(client):
-    return client.get("https://rabota.by/search/vacancy?area=1002&text=shotgun", header=HEADER).text
+    return client.get(URL, params=SHOTGUN_QUERY_PARAMS, header=HEADER).text
