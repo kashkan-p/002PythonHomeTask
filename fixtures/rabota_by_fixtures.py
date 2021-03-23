650e096bb4c6345069fbaebd8cea216194798d5e
pvl <kashkan.p@gmail.com>
2021-03-23 23:52:26 +0300

    Added test_python_occurrence, test_flask_occurrence, test_linux_occurrence

--- fixtures/rabota_by_fixtures.py
+++ fixtures/rabota_by_fixtures.py
@@ -13,6 +13,32 @@ def parser():
     return RabotaByParser()
 
 
-@pytest.fixture
+@pytest.fixture(scope='module')
 def rabota_by_response(client, parser):
     return client.get(parser.URL, params=parser.PYTHON_QUERY_PARAMS, header=parser.HEADER)
+
+
+@pytest.fixture(scope='module')
+def avg_word_occurrence(client, parser, rabota_by_response):
+    last_page = parser.get_last_page_number(rabota_by_response.text)
+
+    vacancy_urls = []
+    for number in range(parser.FIRST_PAGE_NUMBER, last_page):
+        parser.PYTHON_QUERY_PARAMS["page"] = number
+        page = client.get(parser.URL, params=parser.PYTHON_QUERY_PARAMS, header=parser.HEADER).text
+        vacancy_urls.append(parser.parse_vacancy_hrefs(page))
+
+    all_vacancies_urls = parser.get_flat_list(vacancy_urls)
+
+    vacancy_description_raw = []
+    for vacancy_url in all_vacancies_urls:
+        vacancy_description_raw.append(client.get(vacancy_url, header=parser.HEADER).text)
+
+    vacancies_parsed = []
+    for vacancy in vacancy_description_raw:
+        parsed = parser.parse_vacancy_description(vacancy)
+        vacancies_parsed.append(parsed)
+
+    avg_occurrence = parser.count_average_word_occurrence(vacancies_parsed, "python", "linux", "flask")
+
+    return avg_occurrence
--- tests/test_rabota_by.py
+++ tests/test_rabota_by.py
@@ -1,6 +1,27 @@
 def test_rabota_by_connection(rabota_by_response):
-    assert rabota_by_response.status_code == 200
+    assert rabota_by_response.status_code == 200, "The server does not respond"
 
 
 def test_no_results_for_shotgun(rabota_by_response):
-    assert "shotgun" not in rabota_by_response.text
+    assert "shotgun" not in rabota_by_response.text, "Shotgun should not be in search results"
+
+
+def test_python_occurrence(rabota_by_response, avg_word_occurrence):
+    python_count = rabota_by_response.text.lower().count("python")
+    range_low = avg_word_occurrence["python average occurrence"] - 1
+    range_high = avg_word_occurrence["python average occurrence"] + 1
+    assert range_low <= python_count <= range_high, "The word count is out of bounds"
+
+
+def test_flask_occurrence(rabota_by_response, avg_word_occurrence):
+    flask_count = rabota_by_response.text.lower().count("flask")
+    range_low = avg_word_occurrence["flask average occurrence"] - 1
+    range_high = avg_word_occurrence["flask average occurrence"] + 1
+    assert range_low <= flask_count <= range_high, "The word count is out of bounds"
+
+
+def test_linux_occurrence(rabota_by_response, avg_word_occurrence):
+    linux_count = rabota_by_response.text.lower().count("linux")
+    range_low = avg_word_occurrence["linux average occurrence"] - 1
+    range_high = avg_word_occurrence["linux average occurrence"] + 1
+    assert range_low <= linux_count <= range_high, "The word count is out of bounds"
